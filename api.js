572a239efd21191c90b8c86f1cc00a976cf43c25
zxr2441 <46213600+zxr2441@users.noreply.github.com>
2022-01-06 18:00:04 +0800

    Add files via upload

---
+++ api.js
@@ -0,0 +1,1 @@
+(function(){window.BMAP_PROTOCOL = "https"; window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=1.4&ak=9BRa12xTkHVNTsLaITXFGGmF4vZm1QqY"></script>');})();
